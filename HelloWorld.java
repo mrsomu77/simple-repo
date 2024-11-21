7808ffcb7bc1f77f64be145863e5c2fb6a31c91e
Somu <154424982+mrsomu77@users.noreply.github.com>
2024-11-21 22:02:05 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,9 @@
 // HelloWorld.java
-import java.util.Scanner;
+// import java.util.Scanner;
 public class HelloWorld {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
+        // Scanner sc = new Scanner(System.in);
+        int n=5;
         for(int i=0;i<n;i++)
             System.out.println("Hello, World! Manoj & Somu");
     }
